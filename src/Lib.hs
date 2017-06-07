632327ba6330474df02b9de9872d74c941215c20
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-06-07 20:20:07 +0700

    Find relative path

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,18 +16,28 @@ processDir root document = do
     docExist <- doesFileExist document
     if exist && docExist then
         do
-            let docDir =  takeDirectory document
+            let docDir = takeDirectory document
             fullPaths <- readProcess "mdfind" ["-onlyin", root, "-name", ".dll"] ""
             let rootPaths = map (clearRoot root) $ split "\n" fullPaths
-            putStrLn $ head rootPaths
+
+            putStrLn docDir
+
+            let slashs = length $ filter ((==) '/') docDir
+            let leader = if slashs > 0 then
+                            intercalate "" $ replicate slashs "../"
+                        else
+                            "./"
+            let append x = leader ++ x
+            return $ map append rootPaths
     else
-        putStrLn "-- Dir Not Exist --"
+        return []
 
 someFunc :: IO ()
 someFunc = do
     args <- getArgs
     case args of
-        [root, document] ->
-            processDir root document
+        [root, document] -> do
+            results <- processDir root document
+            mapM_ putStrLn results
         _ ->
             putStrLn "-- Invalid Argument --"
