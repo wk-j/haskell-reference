d4379c2068f78fb35a6e492abfac66b7cdc6375f
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-07-01 15:11:48 +0700

    Added #load

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE ViewPatterns #-}
+
+
 module Lib ( someFunc ) where
 
 import System.Environment
@@ -9,14 +12,13 @@ import Data.List
 import Text.Printf
 
 clearRoot :: String -> String -> String
-clearRoot root path =
-    replace root "" path
+clearRoot root = replace root ""
 
 (|>) :: t -> (t -> t) -> t
 (|>) x y = y x
 
-processDir :: String -> String -> IO([String])
-processDir root document = do
+processDir :: String -> String -> String -> IO [String]
+processDir root document extension = do
     exist <- doesDirectoryExist root
     docExist <- doesFileExist document
     if exist && docExist then
@@ -24,34 +26,40 @@ processDir root document = do
             let fullDocDir = takeDirectory document
             let docDir = replace root "" fullDocDir
 
-            fullPaths <- readProcess "mdfind" ["-onlyin", root, "-name", ".dll"] ""
+            fullPaths <- readProcess "mdfind" ["-onlyin", root, "-name", extension] ""
             let notEmpty = split "\n" fullPaths |> filter ((/=) "")
             let rootPaths = notEmpty |> map (clearRoot root)
-            let slashs = length $ filter ((==) '/') docDir 
-            let leader = if slashs > 0 then
-                            intercalate "" $ replicate slashs "../"
-                        else
-                            if docDir == "." then
-                                "./"
-                            else 
-                                "../"
-            -- let append x = replace "//" "/" $ leader ++ x
+            let slashs = length $ filter ((==) '/') docDir
+            let leader = 
+                    if slashs > 0 then 
+                        intercalate "" $ replicate slashs "../"
+                    else 
+                        if docDir == "." then 
+                            "./" 
+                        else 
+                            "../"
             let append x = (leader ++ x) |> replace "//" "/"
             return $ map append rootPaths
     else
         return []
 
 createReference :: String -> String
-createReference r =
-    printf "#r \"%s\"" r
+createReference = printf "#r \"%s\""
+
+createLoad :: String -> String
+createLoad = printf "#load \"%s\""
 
 someFunc :: IO ()
 someFunc = do
     args <- getArgs
     case args of
         [root, document] -> do
-            results <- processDir root document
-            let reference = results |> map createReference
-            mapM_ putStrLn $ reference
+            dlls <- processDir root document ".dll"
+            let reference = dlls |> map createReference
+            mapM_ putStrLn reference
+
+            fsx <- processDir root document ".fs"
+            let loads = fsx |> map createLoad
+            mapM_ putStrLn loads
         _ ->
             putStrLn "-- Invalid Argument --"
